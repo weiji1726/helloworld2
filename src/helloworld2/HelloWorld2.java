8aa51c5e199ea317be4d30566805ecdb4ac3a278
weiji <liangfucheng@yijiupi.cn>
2017-11-29 11:51:53 +0800

    test

--- src/helloworld2/HelloWorld2.java
+++ src/helloworld2/HelloWorld2.java
@@ -2,7 +2,7 @@ package helloworld2;
 
 public class HelloWorld2 {
     public static void main(String[] args) {
-        System.out.println("再次尝试~ ");
+        System.out.println("少林寺方丈~ ");
     }
 
 }
